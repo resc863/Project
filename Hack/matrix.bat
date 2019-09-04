9ba64839aba182dc8c062a5857ac04eb604a0376
resc863 <50689611+resc863@users.noreply.github.com>
2019-09-04 23:00:47 +0900

    Create matrix.bat

---
+++ Hack/matrix.bat
@@ -0,0 +1,9 @@
+@echo off
+title matrix
+color 0a
+
+mode 1000
+
+:matrix
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto matrix
