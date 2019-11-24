a98c6cb10922f3aa5d22b299bc37f8d494e15e15
resc863 <50689611+resc863@users.noreply.github.com>
2019-11-24 15:15:25 +0900

    Update sketch_nov23a.ino

--- sketch_nov23a/sketch_nov23a.ino
+++ sketch_nov23a/sketch_nov23a.ino
@@ -1,19 +1,33 @@
+#include <IRremote.h>
 #include <DHT.h>
 #include <LiquidCrystal.h>
+#include <Servo.h>
 
+int value = 0;
+int RECV_PIN = A1; //define input pin on Arduino
 int piezoPin = 9; // 부저의 ⊕극을 오렌지 보드 8번에 연결 
 int dhtpin = 6;
 int distance;
 int trig = 7;
 int echo = 8;
+unsigned long t;
 
+Servo servo;
 DHT dht(dhtpin, 11);
-
+IRrecv irrecv(RECV_PIN);
+decode_results results;
 LiquidCrystal lcd(0,1,2,3,4,5);
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  servo.attach(9);
   lcd.begin(16,2);
   dht.begin();
+  irrecv.enableIRIn();
+  
+  lcd.setCursor(0,0);
+  
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
   pinMode (piezoPin, OUTPUT); 
@@ -21,24 +35,44 @@ void setup() {
 
 void loop() {
   lcd.clear();
+  t = millis();
+  digitalWrite(LED_BUILTIN, LOW);
   digitalWrite(piezoPin, HIGH);
-  // put your main code here, to run repeatedly:
   int cds = analogRead(A0);
   
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
-
   distance = pulseIn(echo, HIGH)/58;
   
   int h = dht.readHumidity();// 습도를 측정합니다.
-  int t = dht.readTemperature();
+  int temp = dht.readTemperature();
 
-  lcd.setCursor(8,0);
+  if (irrecv.decode(&results)) {
+    lcd.setCursor(5,0);
+    switch (results.value) {
+      case 0xFF6897: 
+        lcd.print(0);
+        value += 30;
+        if (value == 180) {
+          value = 0;            
+        }
+      case 0xFF30CF: 
+        lcd.print(1);
+        value -= 30;
+        if (value == 180) {
+          value = 0;
+        }
+    }
+    servo.write(value);
+    irrecv.resume(); // Receive the next value
+  }
+
+  lcd.setCursor(5,0);
+  lcd.print(t/1000);
+
+  lcd.setCursor(5,1);
   lcd.print(distance);
-
-  lcd.setCursor(8,1);
-  lcd.print(cds);
   
   lcd.setCursor(0,0);
   lcd.print(h);
@@ -46,11 +80,9 @@ void loop() {
   lcd.print("%");
   
   lcd.setCursor(0,1);
-  lcd.print(t);
+  lcd.print(temp);
   lcd.setCursor(2,1);
   lcd.print(" c");
   
-  delay(500);
-  digitalWrite(piezoPin, LOW);
-  delay(500);
+  delay(1000);
 }
