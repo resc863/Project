0f64e7bf13a1dcd21d5226d26d4c78bf83c6a5e2
resc863 <50689611+resc863@users.noreply.github.com>
2020-04-08 13:05:33 +0900

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, t, n, x, y, j, max, m, cnt = 0;
+
+    scanf("%d", &t);
+
+    for (i=0;i<t;i++) {
+        scanf("%d %d", &x, &y);
+        max = 0;
+
+        for (j=x;j<=y;j++) {
+            n = m = 1;
+            cnt = 1;
+
+            while (n != j) {
+                m = m*2;
+
+                if ((n + m) > j) {
+                    m = 1;
+                }
+                n = n + m;
+                cnt++;
+            }
+
+            if (cnt > max) {
+                max = cnt;
+            }
+        }
+        printf("%d\n", max);
+    }
+
+    return 0;
+}
