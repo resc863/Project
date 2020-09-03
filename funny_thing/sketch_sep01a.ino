54b1000e0814fa72b31cef28fd38420b99ccce26
resc863 <50689611+resc863@users.noreply.github.com>
2020-09-03 13:49:49 +0900

    Update sketch_sep01a.ino

--- funny_thing/sketch_sep01a.ino
+++ funny_thing/sketch_sep01a.ino
@@ -1,18 +1,22 @@
 #include "DHT.h"
 
 int value = 0;
-int dhtpin = 6;
-int heat = 0;
+int dhtpin = 3;
+int heat = 8;
+int motor1 = 4;
+int motor2 = 7;
 unsigned long t;
 
-DHT dht(dhtpin);
+DHT dht(dhtpin, DHT11);
 
 void setup() {
   // put your setup code here, to run once:
   dht.begin();
   pinMode(heat, INPUT);
-  pinMode(8, OUTPUT);
-  Serial.begin();
+  pinMode(motor1, OUTPUT);
+  pinMode(motor2, OUTPUT);
+  pinMode(6, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -22,10 +26,20 @@ void loop() {
   int h = dht.readHumidity();
   int temp = dht.readTemperature();
 
-  int light = digitalRead(heat);
+  int light = analogRead(heat);
+  
+  Serial.print("습도: ");
+  Serial.println(h);
 
-  if(light < 200) {
-    digitalWrite(8, HIGH);
+  Serial.print("거리: ");
+  Serial.println(light);
+  
+  if(light > 200) {
+    digitalWrite(motor1, HIGH);
+    digitalWrite(motor2, LOW);
+    digitalWrite(6, HIGH);
   }
   
+  
+  delay(500);
 }
