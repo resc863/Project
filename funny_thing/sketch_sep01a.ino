2a2736a21ed1fb9026d58c7b7b2802c3a492752f
resc863 <50689611+resc863@users.noreply.github.com>
2020-09-04 17:31:04 +0900

    Update sketch_sep01a.ino

--- funny_thing/sketch_sep01a.ino
+++ funny_thing/sketch_sep01a.ino
@@ -5,12 +5,22 @@ int dhtpin = 3;
 int heat = 8;
 int motor1 = 4;
 int motor2 = 7;
+int GP2Y1023 = 2;
+unsigned long pulse = 0;    
+float ugm3 = 0;    
 unsigned long t;
 
 DHT dht(dhtpin, DHT11);
 
  float discomfortIndex(float temp, float humi) {
     return (1.8f*temp)-(0.55*(1-humi/100.0f)*(1.8f*temp-26))+32;
+}
+float pulse2ugm3(unsigned long pulse) {
+  float value = (pulse - 1400) / 14.0;                
+  if (value > 300) {                                        
+    value = 0;                                                
+  }
+  return value;      
 }
 void setup() {
   // put your setup code here, to run once:
@@ -19,6 +29,7 @@ void setup() {
   pinMode(motor1, OUTPUT);
   pinMode(motor2, OUTPUT);
   pinMode(6, OUTPUT);
+  pinMode(GP2Y1023, INPUT); 
   Serial.begin(9600);
 }
 
@@ -44,19 +55,30 @@ void loop() {
   Serial.println(bad);
 
   Serial.println("===========================");
+
+   pulse = pulseIn(GP2Y1023, LOW, 20000); 
+  ugm3 = pulse2ugm3(pulse);                                                      
+    Serial.print(ugm3);                         
+    Serial.println(" ug/m3");
+  
   
   if(light == 0&&bad>80) {
     digitalWrite(motor1, HIGH);
     digitalWrite(motor2, LOW);
     digitalWrite(6, HIGH);
   }
+  else if(light==0&&ugm3>80)
+  {
+    digitalWrite(motor1, HIGH);
+    digitalWrite(motor2, LOW);
+  }
   else {
     digitalWrite(motor1, LOW);
     digitalWrite(motor2, LOW);
     digitalWrite(6, LOW);
   }
+
+  
   
   delay(1000);
-}
-
 }
